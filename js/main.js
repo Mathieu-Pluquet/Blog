337ec68c056958d12ef32bdb7f58fa14b701539d
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-06-26 12:11:54 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 
 
-// hover img
+// onglets
 
+var tabs = document.querySelectorAll('.tabs a')
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click',function(e) {
 
+    var li = this.parentNode
+    var div = this.parentNode.parentNode.parentNode
+
+    if(li.classlist.contains('active')) {
+      return false
+    }
+
+    div.querySelectorAll('.tabs .active').classlist.Remove('active')
+    li.classlist.add('active')
+    div.querySelector('tab-content.active').classlist.Remove('active')
+    div.querySelector(this.getattribute('href')).classlist.add('active')
+  })
+}
 
 
 // formulaire
@@ -14,23 +30,57 @@ function myFunction() {
   } else {
       txt = "You pressed Cancel!";
   }
-  document.getElementById("demo").innerHTML = txt;
+  document.getElementById("demo").textContent = txt;
 }
+
 
 // compteur
 
-const timer = document.getElementById('timer');
-let counter = 0;
-const tableau = [];
-
-function countDown() {
-timer.textContent = counter;
-counter++;
+var compt = document.getElementById("compteur")
+compt.textContent=0;
+function augmenter(){
+  compt.textContent = parseInt(compt.textContent) +1;
 }
+setInterval(augmenter,1000);
 
-setInterval(countDown, 1000);
 
-document.body.addEventListener('click', function() {
-tableau.push(counter);
-console.log(tableau);
-});
+// img hover
+var img = document.getElementsByTagName("img");
+var text = document.getElementsByClassName("bonjour");
+
+for (let i = 0 ; i < img.length ; i++) {
+img[i].addEventListener("mouseover", function() {
+  var alt = img[i].alt;
+  text[i].textContent = alt;
+}
+);
+};
+
+for (let i = 0 ; i < img.length ; i++) {
+img[i].addEventListener("mouseout", function() {
+text[i].textContent = "";
+}
+);
+};
+
+
+
+
+
+
+var img =document.getElementsByTagName("img")
+var text=document.getElementsByClassName("bonjour")
+
+for (let i=0; i <img.length; i++) {
+  img[i].addEventListener("mouseover", function() {
+    var alt =img[i].alt;
+    text[i].textContent =alt;
+  })
+};
+
+
+for (let i=0; i <img.length; i++) {
+  img[i].addEventListener("mouseout", function() {
+    text[i].textContent ="";
+  })
+};
