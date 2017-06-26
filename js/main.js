3b5fdcfdb739a8e151113aaf9bd6ca1ebc6391af
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-06-26 14:56:31 +0200

    finish js

--- js/main.js
+++ js/main.js
@@ -64,23 +64,4 @@ text[i].textContent = "";
 };
 
 
-
-
-
-
-var img =document.getElementsByTagName("img")
-var text=document.getElementsByClassName("bonjour")
-
-for (let i=0; i <img.length; i++) {
-  img[i].addEventListener("mouseover", function() {
-    var alt =img[i].alt;
-    text[i].textContent =alt;
-  })
-};
-
-
-for (let i=0; i <img.length; i++) {
-  img[i].addEventListener("mouseout", function() {
-    text[i].textContent ="";
-  })
-};
+// FONCTION DATE
